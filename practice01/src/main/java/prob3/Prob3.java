11dda727a045a97755fa8bfe5b7478aa24a59555
douzone-busan-bitacademy <83158968+douzone-busan-bitacademy@users.noreply.github.com>
2021-04-28 16:48:16 +0900

    Update Prob3.java

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
+		while (true) {
+			System.out.print( "수를 입력 하세요 : " );
+			
+			int number = scanner.nextInt();
+			int sum = 0;
 
-		Scanner scanner = new Scanner(System.in);
+			for (int i = 0; i <= number; i++) {
+				if (number % 2 == 0 && i % 2 == 0 || number % 2 != 0 && i % 2 != 0) {
+					sum += i;
+				}
+			}
 
-		/* 코드 작성 */
+			System.out.println("결과값: " + sum);
+		}
 		
 		scanner.close();
 	}
