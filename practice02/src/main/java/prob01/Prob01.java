c5991a7af38092b5b862fc1c6ffb9b10d526bb17
douzone-busan-bitacademy <83158968+douzone-busan-bitacademy@users.noreply.github.com>
2021-04-28 16:50:44 +0900

    Update Prob01.java

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -8,8 +8,16 @@ public class Prob01 {
 
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
-		/* 코드 작성 */
-		
+		System.out.print("금액:");
+		int money = scanner.nextInt();
+
+		for (int i = 0; i < MONEYS.length; i++) {
+			int count = money / MONEYS[i];
+			money -= (count * MONEYS[i]);
+
+			System.out.println(MONEYS[i] + "원 : " + count + "개");
+		}
+
 		scanner.close();
  	}
 }
