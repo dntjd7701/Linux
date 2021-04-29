1ab5300f261be7f651f2075406af72d8dc7f4931
douzone-busan-bitacademy <83158968+douzone-busan-bitacademy@users.noreply.github.com>
2021-04-29 16:43:17 +0900

    Create CurrencyConverter.java

---
+++ practice03/src/main/java/prob03/CurrencyConverter.java
@@ -0,0 +1,13 @@
+	private static double rate;
+
+	public static void setRate(double rate) {
+		CurrencyConverter.rate = rate;
+	}
+	
+	public static double toDollar( double krw ) {
+		return krw / rate;
+	}
+	
+	public static double toKRW( double dollar ) {
+		return dollar * rate;
+	}
